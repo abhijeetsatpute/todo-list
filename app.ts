796e776e00a8efd7bb039ad641d1a211cfba0673
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-28 07:02:43 +0530

    adding CORS headers

--- app.ts
+++ app.ts
@@ -10,6 +10,13 @@ app.use( async (ctx, next) => {
   await next();
 });
 
+app.use(async (ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  await next();
+})
+
 app.use(todoRoutes.routes());
 app.use(todoRoutes.allowedMethods());
 
